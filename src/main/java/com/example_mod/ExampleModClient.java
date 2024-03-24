125ffd558af610d255f290c5ed34e05ce9aa5904
sn-blg <snb1g@yandex.ru>
2024-03-24 08:50:38 +0300

    add ExampleModClient

---
+++ src/main/java/com/example_mod/ExampleModClient.java
@@ -0,0 +1,8 @@
+package com.example_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() { }
+}
