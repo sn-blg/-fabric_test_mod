73ae80b2b077cb27083ec67456c93b213d9a1c93
sn-blg <snb1g@yandex.ru>
2024-03-24 17:20:41 +0300

    add ModItems.java

--- src/main/java/com/example_mod/ExampleMod.java
+++ src/main/java/com/example_mod/ExampleMod.java
@@ -1,16 +1,17 @@
 package com.example_mod;
 
+import com.example_mod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "example_mod";
+    public static final String MOD_ID = "example_mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        ModItems.register();
+    }
 }
---
+++ src/main/java/com/example_mod/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.example_mod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import com.example_mod.ExampleMod;
+
+public class ModItems {
+    public static final Item NEW_ITEM = registerItem("new_item", new Item(new FabricItemSettings()));
+    private static Item registerItem(String id, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(ExampleMod.MOD_ID, id), item);
+    }
+    private static Item registerItem(String id, Item item, ItemGroup itemGroup) {
+        Item returnItem = Registry.register(Registries.ITEM, new Identifier(ExampleMod.MOD_ID, id), item);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> entries.add(returnItem));
+        return returnItem;
+    }
+    public static void register() {
+        ExampleMod.LOGGER.debug("Registering items for: " + ExampleMod.MOD_ID);
+    }
+}
